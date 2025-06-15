b8641382d0b7952ab4b5b147446c37f1decb77a9
Stefano Zugni <stefano.zugni@itsincom.it>
2025-06-15 13:38:37 +0200

    Firebase

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/components/app/app.component';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDomWtfgaLPuC5RTqhvZ3RS0jTEsejKwto",
+  authDomain: "follemente-quiz.firebaseapp.com",
+  projectId: "follemente-quiz",
+  storageBucket: "follemente-quiz.firebasestorage.app",
+  messagingSenderId: "1001795213537",
+  appId: "1:1001795213537:web:fabfb775223fb12bf7ffa0",
+  measurementId: "G-9J3NP93CFK"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
